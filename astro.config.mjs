4f9a68e08f99718c0ef53ea014c695a48f565f5f
Omar Caja <omar.caja@hotmail.com>
2026-03-20 14:45:29 +0100

    [OC] - Added new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://omarcaja.github.io',
-  base: '/albinana',
+  site: 'https://farmaciaalbinana.es',
   integrations: [sitemap()],
 });
