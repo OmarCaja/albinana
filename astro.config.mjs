1fc63e0588b55df7efa235a5d96a60f91df14cb0
Omar Caja <omar.caja@hotmail.com>
2026-03-20 10:56:00 +0100

    [OC] - Implementing SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://omarcaja.github.io',
-    base: '/albinana',
+  site: 'https://omarcaja.github.io',
+  base: '/albinana',
+  integrations: [sitemap()],
 });
